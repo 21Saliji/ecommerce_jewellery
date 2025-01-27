5f2ea50b2970ee04e59a49706f03bb070de291fd
Flavia Tsho-Tsho <89797329+FLAVIAFRANCISCA@users.noreply.github.com>
2025-01-27 21:10:10 +0200

    removed the database url in firebaseConfig.js

--- jewellery-store/firebaseConfig.js
+++ jewellery-store/firebaseConfig.js
@@ -4,9 +4,7 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHr2_vCD-B3UXs6zcLQz8WwCbe4sZh-o8",
-  authDomain: "ecommerce-jewellery-store.firebaseapp.com",
-  //databaseURL: "https://ecommerce-jewellery-store-default-rtdb.firebaseio.com",
-  projectId: "ecommerce-jewellery-store",
+  authDomain: "ecommerce-jewellery-store.firebaseapp.com",  projectId: "ecommerce-jewellery-store",
   storageBucket: "ecommerce-jewellery-store.firebasestorage.app",
   messagingSenderId: "1063098206489",
   appId: "1:1063098206489:web:94acf3df340623bac35d88",
